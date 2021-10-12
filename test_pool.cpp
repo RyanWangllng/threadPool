f70dc01ad0289202b6b075f03a797846fa538431
RyanWangllng <wangyouzeng1998@163.com>
2021-10-12 22:24:11 +0800

    demo

---
+++ test_pool.cpp
@@ -0,0 +1,20 @@
+#include "threadPool.h"
+
+void taskFunc(void* arg) {
+    int num = *static_cast<int*>(arg);
+    std::cout << "thread " << std::to_string(pthread_self()) << " is working, number = " << num << std::endl;
+    sleep(1);
+}
+
+int main() {
+    // 创建线程池
+    ThreadPool<int> pool(3, 10);
+    for (int i = 0; i < 100; i++) {
+        int *num = new int(i + 100);
+        pool.addTask(Task<int>(taskFunc, num));
+    }
+
+    sleep(20);
+
+    return 0;
+}
