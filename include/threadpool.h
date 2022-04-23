e2d65b72489297f995a7983f38c1fc797f01502a
RyanWangllng <wangyouzeng1998@163.com>
2022-04-23 18:25:51 +0800

    class Any

--- include/threadpool.h
+++ include/threadpool.h
@@ -1,5 +1,12 @@
-#ifndef THREADPOOL_H
-#define THREADPOOL_H
+//
+//  threadpool.h
+//  threadPool
+//
+//  Created by Ryan Wang.
+//
+
+#ifndef threadpool_h
+#define threadpool_h
 
 #include <iostream>
 #include <vector>
@@ -11,11 +18,54 @@
 #include <functional>
 #include <thread>
 
+// Any类型，可以接收任意的数据类型
+class Any {
+public:
+    Any() = default;
+    ~Any() = default;
+    Any(const Any&) = delete;
+    Any& operator=(const Any&) = delete;
+    Any(Any&&) = default;
+    Any& operator=(Any&&) = default;
+    
+    // 此构造函数让Any类型接收任意其它的数据
+    template<typename T>
+    Any(T data) : base_(std::make_unique<Derive<T>>(data)) {}
+    
+    // 此方法可以将Any对象里面存储的data数据提取出来
+    template<typename T>
+    T cast_() {
+        // 从base_里面找到它所指向的Derive对象，从它里面取出data成员变量
+        Derive<T> *pd = dynamic_cast<Derive<T>>(base_.get());
+        if (pd == nullptr) {
+            throw "type is not match!";
+        }
+        return pd->data_;
+    }
+private:
+    // 基类类型
+    class Base {
+    public:
+        virtual ~Base() = default;
+    };
+    
+    // 派生类类型
+    template<typename T>
+    class Derive : public Base {
+    public:
+        Derive(T data) :data_(data) {}
+
+        T data_; // 保存任意的其他类型
+    };
+private:
+    std::unique_ptr<Base> base_;
+};
+
 // 任务抽象基类
 class Task {
 public:
     // 用户可以自定义任务类型，继承该基类，重写run方法，实现自定义任务处理
-    virtual void run() = 0;
+    virtual Any run() = 0;
 };
 
 // 线程池支持的模式
@@ -41,6 +91,19 @@ public:
 private:
     ThreadFunc func_;
 };
+
+/*
+example:
+ TreadPool pool;
+ pool.start(4);
+ 
+ class MyTask : public Task {
+ public:
+    void run() { ... } // 重写基类run方法
+ };
+ 
+ pool.submitTask(std::make_shared<MyTask>());
+*/
 
 // 线程池类型
 class ThreadPool
@@ -74,7 +137,7 @@ private:
 private:
     PoolMode poolMode_; // 当前线程池的工作模式
 
-    std::vector<Thread*> threads_; // 线程列表
+    std::vector<std::unique_ptr<Thread>> threads_; // 线程列表
     size_t initThreadNums_; // 初始的线程数量
 
     std::queue<std::shared_ptr<Task>> taskQue_; // 任务队列
@@ -86,4 +149,4 @@ private:
     std::condition_variable notEmpty_; // 表示任务队列不空
 };
 
-#endif
+#endif /* threadpool_h */
